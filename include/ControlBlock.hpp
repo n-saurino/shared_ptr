bdf9f38be1ba6bb394728ca911b2249da6e73010
root <nigelsaurino@gmail.com>
2025-02-07 14:20:35 -0600

    AGAIN!

--- include/ControlBlock.hpp
+++ include/ControlBlock.hpp
@@ -8,14 +8,18 @@ public:
     ControlBlock(T* ptr): ptr_{std::move(ptr)}, ref_count_{1}{
 
     }
+
     ~ControlBlock(){
-        
+        delete ptr_;
+        ptr_ = nullptr;
     }
 
-private:
     // need a T pointer
     T* ptr_{}; 
     
     // and an atomic integer for the reference counter
     std::atomic<int> ref_count_{};
+
+private:
+    
 };
--- include/SharedPtr.hpp
+++ include/SharedPtr.hpp
@@ -1,11 +1,16 @@
 #pragma once
 #include "ControlBlock.hpp"
+#include <stdexcept>
 
 template<typename T>
 class SharedPtr
 {
 public:
-    SharedPtr(T* ptr): cb_{std::move(ptr)}{
+    SharedPtr(): cb_{nullptr}{
+        
+    }
+
+    SharedPtr(T* ptr): cb_{ptr ? new ControlBlock<T>{ptr} : nullptr}{
         
     }
 
@@ -13,54 +18,81 @@ public:
     ~SharedPtr(){
         // checks to see if we this is the last SharedPtr pointing to the
         // ControlBlock
-        if(cb_->ref_count_.fetch_sub(1) == 1){
+        if(cb_ && cb_->ref_count_.fetch_sub(1) == 1){
             delete cb_;
         }
         cb_ = nullptr;
     }
     
-    // delete copy constructor
+    // copy constructor
     SharedPtr(const SharedPtr& other){
-        if(this != &other){
-            this->cb_ = other->cb_;
-            this->cb_->ref_count_.fetch_add(1);
-        }
+        this->cb_ = other.cb_;
+        this->cb_->ref_count_.fetch_add(1);
     }
     
-    // delete copy assignment operator
+    // copy assignment operator
     SharedPtr& operator=(const SharedPtr& other){
         if(this == &other){
             return *this;
         }
+        
+        // need to decrement and replace the old cb that it was pointing to
+        if(this->cb_ && this->cb_->ref_count_.fetch_sub(1) == 1){
+            delete cb_;
+            cb_ = nullptr;
+        }
+
+        this->cb_ = other.cb_;
+        this->cb_->ref_count_.fetch_add(1);
+
+        return *this;
     }
     
     // implement move constructor
-    SharedPtr(SharedPtr&& other){
-        if(this != &other){
-            this->cb_ = std::move(other->cb_);
-            other->cb_ = nullptr;
-        }
+    SharedPtr(SharedPtr&& other) noexcept{
+        this->cb_ = std::move(other.cb_);
+        other.cb_ = nullptr;
     } 
     
     // implement move assignment operator
-    SharedPtr& operator=(SharedPtr&& other){
+    SharedPtr& operator=(SharedPtr&& other) noexcept{
         if(this == &other){
             return *this;
         }
-        
-        this->cb_ = std::move(other->cb_);
-        other->cb_ = nullptr;
+        // need to decrement and replace the old cb that it was pointing to
+        if(this->cb_ && this->cb_->ref_count_.fetch_sub(1) == 1){
+            delete cb_;
+            cb_ = nullptr;
+        }
+
+        this->cb_ = std::move(other.cb_);
+        other.cb_ = nullptr;
 
         return *this;
     }
 
     T& operator*(){
+        if(!cb_ || !cb_->ptr_){
+            throw std::runtime_error("Dereferencing nullptr!");
+        }
+
         return *(cb_->ptr_);
     }
     
     T* operator->(){
-        return cb->ptr_;
+        if(!cb_){
+            return nullptr;
+        } 
+        return cb_->ptr_;
     }
+
+    int RefCount(){
+        if(!cb_){
+            return -1;
+        }
+
+        return cb_->ref_count_.load();
+    }    
 
 private:
     ControlBlock<T>* cb_{};
