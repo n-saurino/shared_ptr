063c623249a540338f2ee087570bc9f7a171f82d
root <nigelsaurino@gmail.com>
2025-02-07 11:01:04 -0600

    Just practicing again for a quick exercise

--- include/ControlBlock.hpp
+++ include/ControlBlock.hpp
@@ -1,19 +1,21 @@
 #pragma once
-#include <iostream>
 #include <atomic>
 
 template<typename T>
 class ControlBlock
 {
 public:
-    ControlBlock(T* ptr): ptr_{ptr}, ref_count_{1}{
+    ControlBlock(T* ptr): ptr_{std::move(ptr)}, ref_count_{1}{
 
     }
-
     ~ControlBlock(){
-        delete ptr_;
+        
     }
 
-    T* ptr_{};
+private:
+    // need a T pointer
+    T* ptr_{}; 
+    
+    // and an atomic integer for the reference counter
     std::atomic<int> ref_count_{};
 };
--- include/SharedPtr.hpp
+++ include/SharedPtr.hpp
@@ -5,101 +5,23 @@ template<typename T>
 class SharedPtr
 {
 public:
-    SharedPtr(): cb_ptr_{nullptr}{
-
-    }
-    // instantiates a new control block and points out cb_ptr_ at it
-    // or if T* is nullptr, we set cb_ptr_ to nullptr
-    SharedPtr(T* ptr): cb_ptr_{ptr ? new ControlBlock<T>{ptr} : nullptr}{
-
+    SharedPtr(T* ptr): cb_{ptr}{
+        
     }
 
-    // copy constructor
-    SharedPtr(const SharedPtr& other){
-        // need to decrease the count of the ref_count_ and then
-        // if this is the last ref_count, we need to delete the 
-        // control block
-        if(cb_ptr_ && cb_ptr_->ref_count_.fetch_sub(1) == 1){
-            delete cb_ptr_;
-            cb_ptr_ = nullptr;
-        }
-        cb_ptr_ = other.cb_ptr_;
-        cb_ptr_->ref_count_.fetch_add(1);
-    }
-
-    // move constructor
-    SharedPtr(SharedPtr&& other){
-        // need to decrease the count of the ref_count_ and then
-        // if this is the last ref_count, we need to delete the 
-        // control block
-        if(cb_ptr_ && cb_ptr_->ref_count_.fetch_sub(1) == 1){
-            delete cb_ptr_;
-            cb_ptr_ = nullptr;
-        }
-        cb_ptr_ = other.cb_ptr_;
-        other.cb_ptr_ = nullptr;
-    }
-
+    // make sure you delete allocated memory for ControlBlock ptr
     ~SharedPtr(){
-        if(cb_ptr_ &&  cb_ptr_->ref_count_.fetch_sub(1) == 1){
-            delete cb_ptr_;
-        }
+        
     }
     
-    // copy assignment operator
-    SharedPtr& operator=(const SharedPtr& other){
-        // check for self assignment
-        if(this != &other){
-            if(cb_ptr_ && cb_ptr_->ref_count_.fetch_sub(1) == 1){
-                delete cb_ptr_;
-            }    
-            
-            cb_ptr_ = other.cb_ptr_;
-            if(cb_ptr_){
-                cb_ptr_->ref_count_.fetch_add(1);
-            }
-        }
-        
-        return *this;
-    }
-    
-    // move assignment operator
-    SharedPtr& operator=(SharedPtr&& other) noexcept{
-        if(this != &other){
-            if(cb_ptr_ && cb_ptr_->ref_count_.fetch_sub(1) == 1){
-                delete cb_ptr_;
-            }
-            cb_ptr_ = other.cb_ptr_;
-            other.cb_ptr_ = nullptr;
-        } 
-        
-        return *this;
-    }
-
     T& operator*(){
-        if(!cb_ptr_){
-            throw std::runtime_error("Dereferencing a nullptr");        
-        }
-        
-        return *(cb_ptr_->ptr_);
-    }
-
-    T* operator->(){
-        if(cb_ptr_){
-            return cb_ptr_->ptr_;
-        } 
-        
-        return nullptr;
+        return *(cb_->ptr_);
     }
     
-    int RefCount() const{
-        if(cb_ptr_){
-            return cb_ptr_->ref_count_.load();
-        }
-        
-        return -1;
+    T* operator->(){
+        return cb->ptr_;
     }
 
 private:
-    ControlBlock<T>* cb_ptr_{};
+    ControlBlock* cb_{};
 };
